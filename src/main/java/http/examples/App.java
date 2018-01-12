e96224a49a34043d25c9828d68cfe8d1d3530eb5
Krzysztof Ambroziak <krzysztof.ambroziak2@bm.pl>
2018-01-12 16:37:37 +0100

    unused import remove

--- src/main/java/http/examples/App.java
+++ src/main/java/http/examples/App.java
@@ -1,22 +1,6 @@
 package http.examples;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.EntityBuilder;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.HttpClients;
-
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
+import java.io.IOException;
 
 /**
  * Hello world!
--- src/test/java/http/examples/AppTest.java
+++ src/test/java/http/examples/AppTest.java
@@ -17,9 +17,7 @@ import org.junit.Test;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
