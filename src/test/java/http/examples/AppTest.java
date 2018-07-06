d967e1ca9ff5d14ec2e1d4f3e028b98f6e494659
Krzysztof Ambroziak <krzysztof.ambroziak2@bm.pl>
2018-07-06 22:01:59 +0200

    Finalna wersja

--- src/test/java/http/examples/AppTest.java
+++ src/test/java/http/examples/AppTest.java
@@ -1,14 +1,18 @@
 package http.examples;
 
+import com.google.gson.*;
 import org.apache.http.Header;
+import org.apache.http.HttpEntity;
 import org.apache.http.client.entity.EntityBuilder;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.BufferedReader;
@@ -131,32 +135,120 @@ public class AppTest {
 
     @Test
     public void testGsonParse() {
+        String json = "{\"firstName\":\"Krzysztof\", \"lastName\":\"Ambroziak\"}";
 
+        JsonParser jsonParser = new JsonParser();
+        JsonElement jsonElement = jsonParser.parse(json);
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+        String lastName = jsonObject.get("lastName").getAsString();
+
+        System.out.println(lastName);
     }
 
     @Test
     public void testBuildSentence() throws IOException {
+        CloseableHttpClient client = HttpClientBuilder.create().build();
+        HttpGet httpGet = new HttpGet("https://restcountries.eu/rest/v2/name/poland");
+        CloseableHttpResponse response = client.execute(httpGet);
 
+        int statusCode = response.getStatusLine().getStatusCode();
+
+        System.out.println("Status: " + statusCode);
+        String statusDescription = response.getStatusLine().getReasonPhrase();
+        System.out.println(statusDescription);
+
+        String jsonAsString = new BasicResponseHandler().handleResponse(response);
+
+        JsonParser jsonParser = new JsonParser();
+        JsonElement rootElement = jsonParser.parse(jsonAsString);
+
+        JsonArray rootJsonArray = rootElement.getAsJsonArray();
+
+        JsonObject rootJsonObject = rootJsonArray.get(0).getAsJsonObject();
+        StringBuilder sentenceBuilder = new StringBuilder("Polska ma ");
+        int population = rootJsonObject.get("population").getAsInt();
+        sentenceBuilder.append(population).append(" ludnosci, graniczy z:");
+        JsonArray borders = rootJsonObject.get("borders").getAsJsonArray();
+
+        for (JsonElement element : borders) {
+            sentenceBuilder.append(element.getAsString()).append(",");
+        }
+        sentenceBuilder.append(" a po włosku to: ");
+        JsonObject translations = rootJsonObject.get("translations").getAsJsonObject();
+        String it = translations.get("it").getAsString();
+
+        sentenceBuilder.append(it);
+
+        System.out.println(sentenceBuilder.toString());
+
+        //Polska ma xxx ludnosci, graniczy z:..... a po włosku to:....
 
     }
 
     @Test
     public void buildJsonFromObject() {
+        Student student = new Student();
+        student.setFirstName("Krzysztof");
+        student.setLastName("Ambroziak");
+        student.setAge(18);
+
+        String studentAsJson = new Gson().toJson(student);
+
+        System.out.println(studentAsJson);
 
     }
 
     @Test
     public void buildObjectFromJson() {
+        Student student = new Student();
+        student.setFirstName("Krzysztof");
+        student.setLastName("Ambroziak");
+        student.setAge(18);
+
+        String studentAsJson = new Gson().toJson(student);
+
+        Student studentFromJson = new Gson().fromJson(studentAsJson, Student.class);
+
+        Assert.assertEquals(student.getLastName(), studentFromJson.getLastName());
+        Assert.assertEquals(student.getFirstName(), studentFromJson.getFirstName());
+        Assert.assertEquals(student.getAge(), studentFromJson.getAge());
 
     }
 
     @Test
     public void sendStudentData() throws IOException {
+        Student student = new Student();
+        student.setFirstName("K");
+        student.setLastName("A");
+        student.setAge(18);
 
+        CloseableHttpClient client = HttpClientBuilder.create().build();
+        HttpPost httpPost = new HttpPost("https://protected-cove-60658.herokuapp.com/students");
+
+        String studentAsJson = new Gson().toJson(student);
+        System.out.println(studentAsJson);
+        HttpEntity entity = EntityBuilder.create().setText(studentAsJson)
+                .setContentType(ContentType.APPLICATION_JSON).build();
+
+        httpPost.setEntity(entity);
+
+        CloseableHttpResponse response = client.execute(httpPost);
+
+        String responseAsString = new BasicResponseHandler().handleResponse(response);
+
+        System.out.println(responseAsString);
     }
 
     @Test
     public void getStudentData() throws IOException {
+        CloseableHttpClient client = HttpClientBuilder.create().build();
+        HttpGet httpGet = new HttpGet("https://protected-cove-60658.herokuapp.com/students");
+
+        CloseableHttpResponse response = client.execute(httpGet);
+
+        String responseAsString = new BasicResponseHandler().handleResponse(response);
+
+        System.out.println(responseAsString);
 
     }
 
